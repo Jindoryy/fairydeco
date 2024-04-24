3f8f53d0668a16782d347504fba304b0fbd1942c
김선혁 <kkn1312@naver.com>
2024-04-24 14:50:11 +0900

    [Fix] service register 코드 추가

--- server_main/src/main/java/com/a402/fairydeco/domain/book/service/BookService.java
+++ server_main/src/main/java/com/a402/fairydeco/domain/book/service/BookService.java
@@ -1,6 +1,12 @@
 package com.a402.fairydeco.domain.book.service;
 
+import com.a402.fairydeco.domain.book.dto.BookRegister;
+import com.a402.fairydeco.domain.book.dto.BookStory;
+
+import java.io.IOException;
+
 public interface BookService {
 
+    public BookStory register(BookRegister bookRegister) throws IOException;
 
 }
