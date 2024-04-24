ef9d147756bec7b3b4c887ba6e12de4f71a7fb25
박지원 <wldnjs11234@naver.com>
2024-04-24 14:14:22 +0900

    [Chore] tailwind, prettier, daisyUI 추가

---
+++ client/prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Options} */
+
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+};
+
+module.exports = {
+  ...config,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
