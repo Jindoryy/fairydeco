7a9725e9c3e2fcb533962f6ef4b9988e97e94bd5
Yeonhwa <itidnsld@naver.com>
2024-04-29 14:23:46 +0900

    [Feat] 마이페이지 css 시작

--- client/src/app/components/header.jsx
+++ client/src/app/components/header.jsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { SignIn } from '@phosphor-icons/react/dist/ssr'
 
 export default function Header() {
     return (
-        <div className="bg-customYellow flex h-20 items-center justify-between px-4">
+        <div className="flex h-20 items-center justify-between bg-customYellow px-4">
             <div className="relative h-5/6 w-40">
                 <Image
                     src="/image/logo.png"
@@ -12,7 +13,9 @@ export default function Header() {
                     objectFit="cover"
                 />
             </div>
-            <SignIn size={40}></SignIn>
+            <Link href="/login">
+                <SignIn size={40}></SignIn>
+            </Link>
         </div>
     )
 }
--- client/src/app/mypage/page.jsx
+++ client/src/app/mypage/page.jsx
@@ -1,3 +1,92 @@
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
+import Header from '../components/header'
+
 export default function Mypage() {
-    return <div>Mypage 입니다.</div>
+    // 부모님의 정보를 담을 상태를 생성합니다.
+    const [userInfo, setUserInfo] = useState({
+        user: {
+            userLoginId: 'kimmommy',
+            userName: '김엄마',
+            userGender: 'WOMAN',
+            userBirth: '1988-09-18',
+        },
+        childResponseList: [
+            {
+                childId: 1,
+                childName: '김아들',
+                childBirth: '2000-01-01',
+                childGender: 'MAN',
+                bookList: [
+                    {
+                        bookId: 1,
+                        bookName: '건희의 모험',
+                        bookMaker: '김아들',
+                        bookPictureUrl: '',
+                        bookCoverUrl: '',
+                        bookCreatedAt: '',
+                        bookComplete: 'STORY',
+                    },
+                    {
+                        bookId: 2,
+                        bookName: '건희의 모험2',
+                        bookMaker: '김아들',
+                        bookPictureUrl: null,
+                        bookCoverUrl: '',
+                        bookCreatedAt: '',
+                        bookComplete: 'STORY',
+                    },
+                ],
+            },
+            {
+                childId: 2,
+                childName: '김딸',
+                childBirth: '2000-01-01',
+                childGender: 'WOMAN',
+                bookList: [
+                    {
+                        bookId: 1,
+                        bookName: '건희의 모험',
+                        bookMaker: '김딸',
+                        bookPictureUrl: '',
+                        bookCoverUrl: '',
+                        bookCreatedAt: '',
+                        bookComplete: 'STORY',
+                    },
+                    {
+                        bookId: 2,
+                        bookName: '건희의 모험2',
+                        bookMaker: '김딸',
+                        bookPictureUrl: null,
+                        bookCoverUrl: '',
+                        bookCreatedAt: '',
+                        bookComplete: 'STORY',
+                    },
+                ],
+            },
+        ],
+    })
+
+    return (
+        <div>
+            <Header></Header>
+            <div className="py-5 text-8xl">우리 가족</div>
+            <div className="flex justify-between py-5 text-5xl">
+                <div className="px-5">
+                    <div>부모님</div>
+                    {/* 부모님의 아이디와 학습 진행도를 표시 */}
+                    <div>
+                        <div>아이디: {userInfo.user.userLoginId}</div>
+                        <div>이름: {userInfo.user.userName}</div>
+                        <div>성별: {userInfo.user.userGender}</div>
+                        <div>생년월일: {userInfo.user.userBirth}</div>
+                    </div>
+                    <div>목소리 학습 중: 75%</div>
+                </div>
+                <div className="px-5">아이</div>
+            </div>
+        </div>
+    )
 }
