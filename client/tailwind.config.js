3443b815d6ddaf04edef6ad8b93f11046ea34f8e
박지원 <wldnjs11234@naver.com>
2024-04-29 13:03:45 +0900

    [Feat] 글씨체적용, 공용 색상 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,11 +10,19 @@ module.exports = {
         extend: {
             colors: {
                 customYellow: '#FCF4DB',
+                customPink: '#FCEAE9',
+                customPurple: '#CA54C9',
+                customGreen: '#A1C09F',
+                customRed: '#DC2823',
+                customGray: '#807A7A',
             },
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
                     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            fontFamily: {
+                ourFont: ['TTHakgyoansimMonggeulmonggeulR'],
             },
         },
     },
