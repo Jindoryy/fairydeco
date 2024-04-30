9b3561d8abff426f0cf36f7dc4ac2e3f60e23268
Yeonhwa <itidnsld@naver.com>
2024-04-30 15:01:51 +0900

    [Feat] 동화페이지 css 시작

--- client/src/app/book/page.jsx
+++ client/src/app/book/page.jsx
@@ -1,7 +1,42 @@
-export default function Book() {
+'use client'
+
+import { useRef, useEffect } from 'react'
+import $ from 'jquery'
+import 'turn.js'
+import styles from './components/Flipbook.module.css' // CSS 모듈 임포트
+
+const Flipbook = ({ options = {} }) => {
+    const bookRef = useRef(null)
+
+    useEffect(() => {
+        const book = bookRef.current
+        if (book) {
+            $(book).turn(options)
+
+            const handleResize = () => {
+                book.style.width = ''
+                book.style.height = ''
+                $(book).turn('size', book.clientWidth, book.clientHeight)
+            }
+
+            window.addEventListener('resize', handleResize)
+
+            return () => {
+                $(book).turn('destroy')
+                window.removeEventListener('resize', handleResize)
+            }
+        }
+    }, [options])
+
     return (
-        <>
-            <h1>Bookpage 입니다</h1>
-        </>
+        <div className={styles.flipbook} ref={bookRef}>
+            {pages.map((page, index) => (
+                <div key={index} className={styles.page}>
+                    <img src={page} draggable="false" alt="" />
+                </div>
+            ))}
+        </div>
     )
 }
+
+export default Flipbook
--- client/src/app/mypage/page.jsx
+++ client/src/app/mypage/page.jsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import Link from 'next/link'
 import Header from '../components/header'
+import { PencilSimpleLine } from '@phosphor-icons/react/dist/ssr'
 
 export default function Mypage() {
     // 부모님의 정보를 담을 상태를 생성합니다.
@@ -79,12 +79,13 @@ export default function Mypage() {
     }
 
     return (
-        <div className="font-ourFont">
+        <div className="font-ourFont tracking-wider">
             <Header></Header>
-            <div className="mx-5 py-5 text-5xl">우리 가족</div>
-            <div className="bg-customLigntGreen mx-7 flex h-1/3 justify-around rounded-lg px-5 py-5 text-3xl">
-                <div className="flex-grow px-5">
-                    <div className="text-4xl">부모님</div>
+            <div className="mx-5 py-10 text-6xl">우리 가족</div>
+            <div className="mx-7 flex h-[550px] justify-around rounded-lg bg-customLigntGreen p-10 text-3xl">
+                <div className="mx-5 flex-grow pr-5">
+                    <div className="mt-5 text-5xl">부모님</div>
+                    <br />
                     {/* 부모님의 아이디와 학습 진행도를 표시 */}
                     <div>
                         <div>아이디 : {userInfo.user.userLoginId}</div>
@@ -92,79 +93,101 @@ export default function Mypage() {
                         <div>성별 : {userInfo.user.userGender}</div>
                         <div>생년월일 : {userInfo.user.userBirth}</div>
                     </div>
-                    <div>
-                        <span className="text-customOrange">목소리</span> 학습
-                        중 :{' '}
-                        <span className="text-customOrange">
-                            {voiceLearningPercent}%
-                        </span>
-                    </div>
-                    {/* 게이지바 구현 */}
-                    <div className="border-customOrange h-10 w-full rounded-lg border-4 border-solid bg-white">
-                        <div
-                            className="bg-customOrange h-full rounded-l"
-                            style={{ width: `${voiceLearningPercent}%` }} // 퍼센트에 따라 너비 조정
-                        ></div>
+                    <br />
+                    <div className="mr-12">
+                        <div className=" text-4xl">
+                            <span className="text-customOrange">목소리</span>{' '}
+                            학습 중 :{' '}
+                            <span className="text-customOrange">
+                                {voiceLearningPercent}%
+                            </span>
+                        </div>
+                        {/* 게이지바 구현 */}
+                        <div className="h-[80px] w-full rounded-lg border-4 border-solid border-customOrange bg-white">
+                            <div
+                                className="h-full rounded-l bg-customOrange"
+                                style={{ width: `${voiceLearningPercent}%` }} // 퍼센트에 따라 너비 조정
+                            ></div>
+                        </div>
                     </div>
                 </div>
-                {/* 탭 영역을 오른쪽 정렬 */}
-                <div className="flex-grow px-5">
-                    {/* 탭 목록 */}
+                {/* 탭 영역 */}
+                <div className="mx-5 my-5 flex-grow px-5">
                     <div
                         role="tablist"
-                        className="tabs tabs-lifted flex justify-end"
+                        className="tabs tabs-lifted mr-4 flex justify-end "
                     >
                         {userInfo.childResponseList.map((child, index) => (
-                            <a
-                                role="tab"
-                                className={`tab ${
+                            <input
+                                type="radio"
+                                className={`tab mx-[3px] h-[50px] !w-[100px] !border-x-[3px] !border-t-[3px] !border-customBlueBorder text-lg ${
                                     selectedTab === index
-                                        ? 'bg-customBlue border-customBlueBorder border-4 border-solid'
-                                        : 'border-customBlueBorder border-4 border-solid bg-white'
+                                        ? 'tab-active  !bg-customBlue'
+                                        : 'bg-white'
                                 }`}
+                                checked={selectedTab === index}
+                                aria-label={child.childName}
                                 onClick={() => handleTabClick(index)}
                                 key={child.childId}
-                            >
-                                {child.childName}
-                            </a>
+                            />
                         ))}
                         {/* 항상 추가되는 "기타" 탭 */}
-                        <a
-                            role="tab"
-                            className={`tab ${
+                        <input
+                            type="radio"
+                            className={`tab h-[50px] !w-[100px] !border-x-[3px] !border-t-[3px] !border-customBlueBorder ${
                                 selectedTab ===
                                 userInfo.childResponseList.length
-                                    ? 'bg-customBlue border-customBlueBorder border-4 border-solid'
-                                    : 'border-customBlueBorder border-4 border-solid bg-white'
+                                    ? 'tab-active !bg-customBlue'
+                                    : 'bg-white'
                             }`}
+                            checked={
+                                selectedTab ===
+                                userInfo.childResponseList.length
+                            }
+                            aria-label="+"
                             onClick={() =>
                                 handleTabClick(
                                     userInfo.childResponseList.length
                                 )
                             }
-                        >
-                            +
-                        </a>
+                        />
                     </div>
 
-                    {/* 선택된 탭의 아이 정보 */}
-                    <div className="bg-customBlue border-customBlueBorder rounded-lg border-2 border-solid px-3 py-5 text-3xl">
-                        <div className="text-4xl">아이</div>
-                        <div>
-                            이름 :{' '}
-                            {userInfo.childResponseList[selectedTab].childName}
-                        </div>
-                        <div>
-                            성별 :{' '}
-                            {
-                                userInfo.childResponseList[selectedTab]
-                                    .childGender
-                            }
-                        </div>
-                        <div>
-                            생년월일 :{' '}
-                            {userInfo.childResponseList[selectedTab].childBirth}
-                        </div>
+                    {/* 탭 컨텐츠 */}
+                    <div
+                        role="tabpanel"
+                        className="h-[350px] rounded-box border-4 border-customBlueBorder bg-customBlue p-10"
+                    >
+                        {selectedTab < userInfo.childResponseList.length ? (
+                            <div>
+                                <div className="text-5xl">아이</div>
+                                <br />
+                                <div>
+                                    이름:{' '}
+                                    {
+                                        userInfo.childResponseList[selectedTab]
+                                            .childName
+                                    }
+                                </div>
+                                <PencilSimpleLine size={32} />
+                                <div>
+                                    성별:{' '}
+                                    {
+                                        userInfo.childResponseList[selectedTab]
+                                            .childGender
+                                    }
+                                </div>
+                                <div>
+                                    생년월일:{' '}
+                                    {
+                                        userInfo.childResponseList[selectedTab]
+                                            .childBirth
+                                    }
+                                </div>
+                            </div>
+                        ) : (
+                            <div>아이를 추가할거예요</div>
+                        )}
                     </div>
                 </div>
             </div>
