764413775f146234dfde893092acbf4867d4b7f3
박지원 <wldnjs11234@naver.com>
2024-04-30 13:13:29 +0900

    [Refactor] 헤더 layout으로 수정

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from './components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Header />
+                {children}
+            </body>
         </html>
     )
 }
