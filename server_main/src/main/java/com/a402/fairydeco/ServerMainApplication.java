446fe1f03ec1c9c95d2683ec95c1a95cb8394d4f
김선혁 <kkn1312@naver.com>
2024-05-02 16:34:07 +0900

    [Feat] 파인튜닝 적용 + Cors 에러 bean 등록

--- server_main/src/main/java/com/a402/fairydeco/ServerMainApplication.java
+++ server_main/src/main/java/com/a402/fairydeco/ServerMainApplication.java
@@ -2,7 +2,10 @@ package com.a402.fairydeco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +13,16 @@ public class ServerMainApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ServerMainApplication.class, args);
+  }
+  @Bean
+  public WebMvcConfigurer corsConfigure() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","PUT","POST","DELETE");
+      }
+
+    };
   }
 
 }
