fc3aa3a84c0ee22348d2a36820e5fd2ba16129b1
박지원 <wldnjs11234@naver.com>
2024-05-03 13:41:11 +0900

    [Fix] 레이아웃 에러 수정

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,23 +1,17 @@
 import './globals.css'
-import Header from './components/header'
-import Image from 'next/image'
-import babyIcon from '../../public/image/babylogo.png'
 
 export const metadata = {
     title: '동화꾸미기',
     description: '아이와 함께 AI로 동화를 꾸며보세요!',
     icons: {
-        icon: babyIcon,
+        icon: '/image/babylogo.png',
     },
 }
 
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
-            <body className="font-ourFont">
-                <Header />
-                {children}
-            </body>
+            <body className="font-ourFont">{children}</body>
         </html>
     )
 }
