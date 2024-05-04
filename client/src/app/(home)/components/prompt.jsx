11e208cf8a2ca8e55131316a38dddfd8aeb832f2
이건희 <dlrjsgml109@gmail.com>
2024-05-04 13:55:02 +0900

    [Infra] Next.js env 테스트

--- client/src/app/(home)/components/prompt.jsx
+++ client/src/app/(home)/components/prompt.jsx
@@ -15,8 +15,7 @@ import {
 
 export default function Prompt() {
     const router = useRouter()
-    // const apiUrl = process.env.NEXT_PUBLIC_API_URL
-    const apiUrl = 'https://fairydeco.site/api'
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL
     const [writer, setWriter] = useState('')
     const [kids, setKids] = useState([])
     const [uploadImage, setUploadImage] = useState(false)
