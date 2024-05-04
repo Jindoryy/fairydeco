0f8caf454b147d8d8c552ff7855ea662a4cf8392
youngjin <poweryj1@naver.com>
2024-05-04 23:00:38 +0900

    [Fix] 목소리 녹음파일 저장 api 수정 - VoiceRegistRequset에 @Setter 추가 [Related to] S10P31A402-260

--- server_main/src/main/java/com/a402/fairydeco/domain/voice/dto/VoiceRegistRequset.java
+++ server_main/src/main/java/com/a402/fairydeco/domain/voice/dto/VoiceRegistRequset.java
@@ -2,9 +2,11 @@ package com.a402.fairydeco.domain.voice.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class VoiceRegistRequset {
 
