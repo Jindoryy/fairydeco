0913e71ec8554956eb41dd6a16132a3fc39f06cd
이건희 <dlrjsgml109@gmail.com>
2024-05-04 19:27:20 +0900

    [Infra] Next.js env 테스트

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
     env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://fairydeco.site/api",
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
     },
   };
 
