ff750b19be0db1970d6f67d7d259ba7652eeea77
이건희 <dlrjsgml109@gmail.com>
2024-05-06 18:22:13 +0900

    [Fix] swagger 주소 수정

--- server_express/src/config/swaggerConfig.js
+++ server_express/src/config/swaggerConfig.js
@@ -5,7 +5,6 @@ const swaggerUi = require('swagger-ui-express');
 const port = process.env.EXPRESS_PORT || 3000;
 const serverUrl = `process.env.EXPRESS_ACCESS_SERVER_URL:${port}` || `http://localhost:${port}`;
 
-
 const swaggerDefinition = {
     openapi: '3.0.0',
     info: {
