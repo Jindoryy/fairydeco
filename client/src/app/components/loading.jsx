a8ac2a388b8ca76e511e3f126b11e9b922e8f6b4
박지원 <wldnjs11234@naver.com>
2024-05-07 10:36:31 +0900

    [Feat] 로딩 컴포넌트 추가 및 css수정

---
+++ client/src/app/components/loading.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import BookGif from '../../../public/image/books.gif'
+
+export default function Loading() {
+    console.log('loading...')
+    return (
+        <div className="flex h-[600px] w-[100%] flex-col items-center justify-center bg-white text-3xl">
+            <Image src={BookGif} alt="Loading..." />
+            이야기를 만들고 있어요! 잠시만 기다려주세요!
+        </div>
+    )
+}
