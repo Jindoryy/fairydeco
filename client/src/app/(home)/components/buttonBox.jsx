c0a3c3301effbc15ea897177712dbb271153dda7
박지원 <wldnjs11234@naver.com>
2024-05-09 11:37:56 +0900

    [Feat] 메인페이지 수정 완료

--- client/src/app/(home)/components/buttonBox.jsx
+++ client/src/app/(home)/components/buttonBox.jsx
@@ -5,6 +5,8 @@ import MakeBookImage from '../../../../public/image/makebook.png'
 import PaintingImage from '../../../../public/image/painting.png'
 import Shelf from '../../../../public/image/shelf.png'
 import House from '../../../../public/image/house.png'
+import Horse from '../../../../public/image/horse.png'
+import ProfileImage from '../../../../public/image/profilebook.png'
 import { useRouter } from 'next/navigation'
 import { Baby } from '@phosphor-icons/react/dist/ssr'
 
@@ -14,39 +16,41 @@ export default function ButtonBox() {
     const goProfile = () => {
         setTimeout(() => {
             router.push('/profile')
-        }, 300)
+        }, 200)
     }
 
     const goMakeBook = () => {
         setTimeout(() => {
             router.push('/makebook')
-        }, 300)
+        }, 200)
     }
 
     const goBookList = () => {
         setTimeout(() => {
             router.push('/bookList')
-        }, 300)
+        }, 200)
     }
     const goMypage = () => {
         setTimeout(() => {
             router.push('/mypage')
-        }, 300)
+        }, 200)
     }
     const goPainting = () => {
         setTimeout(() => {
             router.push('/painting')
-        }, 300)
+        }, 200)
     }
     return (
         <div>
-            <button
-                className="btn btn-ghost relative  ml-2 mt-2 h-10 w-1/12 pt-2 align-middle text-lg font-thin text-white hover:bg-transparent focus:bg-transparent"
-                onClick={goProfile}
-            >
-                <Baby size={50} weight="fill" style={{ color: 'white' }} />
-                아이 선택
-            </button>
+            <div className="mr-4 flex justify-end">
+                <button
+                    className="btn btn-ghost relative ml-2 mt-2 h-10 w-1/12 pt-2 align-middle text-lg font-thin text-white hover:rotate-12 hover:bg-transparent focus:bg-transparent"
+                    onClick={goProfile}
+                >
+                    <Image src={Horse} alt="아이 선택" />
+                    아이 선택
+                </button>
+            </div>
             <div className="flex h-auto w-full items-center justify-between pl-20 pr-80">
                 <div className="relative flex flex-col items-center justify-center">
                     <button
