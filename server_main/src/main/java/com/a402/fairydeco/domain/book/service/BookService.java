86e65865e637b3eb364cde26fe4d6fd4dfcc78f8
김선혁 <kkn1312@naver.com>
2024-05-09 16:01:20 +0900

    [Fix] 스토리 코드 수정

--- server_main/src/main/java/com/a402/fairydeco/domain/book/service/BookService.java
+++ server_main/src/main/java/com/a402/fairydeco/domain/book/service/BookService.java
@@ -180,6 +180,7 @@ public class BookService {
                 RestTemplate restTemplate = new RestTemplate();
 
                 try {
+                    System.out.println(request.getBookId() +" !! "+request.getPageId());
                     ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
                     // 응답이 정상이면 true 반환
                     if (response.getStatusCode() == HttpStatus.OK) {
