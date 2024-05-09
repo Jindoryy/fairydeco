8dabfa5d7649754acae458fd8da5185c290b70a4
박지원 <wldnjs11234@naver.com>
2024-05-09 15:46:12 +0900

    [Feat] 업로드 페이지 css 완료

--- client/src/app/makebook/uploadfile/components/uploadBox.jsx
+++ client/src/app/makebook/uploadfile/components/uploadBox.jsx
@@ -17,23 +17,43 @@ export default function UploadBox() {
         }
         reader.readAsDataURL(file)
     }
+    const makeBook = () => {
+        //동화만들기
+    }
     return (
-        <div className="shadow-innerShadow bg-customDarkYellow hover:bg-customDarkYellow relative top-24 m-auto h-[500px] w-2/3 rounded-3xl border-none p-4 text-5xl font-thin">
-            <div className="flex h-full min-h-[190px] w-full items-center justify-center rounded-2xl ">
+        <div className="shadow-innerShadow bg-customDarkYellow hover:bg-customDarkYellow relative top-16 m-auto flex h-[500px] w-2/3 flex-col items-center justify-center rounded-3xl border-none p-4 text-5xl font-thin">
+            <div className="flex h-4/5 min-h-[190px] w-full items-center justify-center rounded-2xl ">
                 <input
                     type="file"
-                    className="file-input file-input-bordered file-input-accent mr-4 w-full max-w-xs"
+                    className="file-input file-input-bordered file-input-accent mr-8 w-full max-w-xs font-thin"
                     onChange={handleFileChange}
                 />
                 {kidImageView && (
-                    <Image
-                        src={kidImageView}
-                        alt="Kid Image"
-                        width={300}
-                        height={300}
-                    />
+                    <div className="overflow-hidden">
+                        <Image
+                            src={kidImageView}
+                            alt="Kid Image"
+                            width={400}
+                            height={400}
+                            style={{
+                                objectFit: 'cover',
+                                borderRadius: '30px',
+                            }}
+                            relative
+                        />
+                    </div>
                 )}
             </div>
+            {kidImageView && (
+                <div>
+                    <button
+                        className="btn w-full border-none bg-customBlueBorder text-2xl font-thin hover:bg-customBlueBorder"
+                        onClick={makeBook}
+                    >
+                        동화 만들기
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
