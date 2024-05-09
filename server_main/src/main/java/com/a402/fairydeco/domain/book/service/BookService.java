15caf340d26c52227329dd990737fd798730ab9f
김선혁 <kkn1312@naver.com>
2024-05-09 15:26:28 +0900

    [Fix] 스토리 코드 수정

--- server_main/src/main/java/com/a402/fairydeco/domain/book/service/BookService.java
+++ server_main/src/main/java/com/a402/fairydeco/domain/book/service/BookService.java
@@ -8,15 +8,18 @@ import com.a402.fairydeco.domain.child.entity.Child;
 import com.a402.fairydeco.domain.child.repository.ChildRepository;
 import com.a402.fairydeco.domain.page.dto.PageAllListResponse;
 import com.a402.fairydeco.domain.page.dto.PageListResponse;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -29,6 +32,7 @@ public class BookService {
 
     @Value("${EXPRESS_SERVER_URL}")
     private String EXPRESS_SERVER_URL;
+
     public List<BookMainListResponse> findBookMainList(Integer bookId) {
 
         List<BookMainListResponse> bookMainListResponses = new ArrayList<>();
@@ -41,7 +45,7 @@ public class BookService {
             }
         } else {
             Book bookExist = bookRepository.findById(bookId)
-                .orElseThrow(()-> new IllegalArgumentException("Book Not Found"));
+                    .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
 
             List<Book> bookList = bookRepository.findTop20ByCompleteAndIdLessThanOrderByIdDesc(CompleteStatus.COMPLETE, bookId);
 
@@ -56,11 +60,11 @@ public class BookService {
     private BookMainListResponse buildBookMainListResponse(Book book) {
 
         return BookMainListResponse.builder()
-            .bookId(book.getId())
-            .bookName(book.getName())
-            .bookMaker(book.getMaker())
-            .bookCoverUrl(book.getCoverUrl())
-            .build();
+                .bookId(book.getId())
+                .bookName(book.getName())
+                .bookMaker(book.getMaker())
+                .bookCoverUrl(book.getCoverUrl())
+                .build();
     }
 
     public List<BookLandingListResponse> findBookLandingList() {
@@ -70,8 +74,8 @@ public class BookService {
 
         for (Book book : bookList) {
             bookLandingListResponses.add(BookLandingListResponse.builder()
-                .bookCoverUrl(book.getCoverUrl())
-                .build());
+                    .bookCoverUrl(book.getCoverUrl())
+                    .build());
         }
 
         return bookLandingListResponses;
@@ -80,30 +84,30 @@ public class BookService {
     public BookTitleUpdateResponse modifyBookTitle(BookTitleUpdateRequest bookTitleUpdateRequest) {
 
         Book book = bookRepository.findById(bookTitleUpdateRequest.getBookId())
-            .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
+                .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
 
         book.updateBookName(bookTitleUpdateRequest.getBookName());
 
         bookRepository.save(book);
 
         return BookTitleUpdateResponse.builder()
-            .bookName(book.getName())
-            .build();
+                .bookName(book.getName())
+                .build();
     }
 
     public List<BookChildPictureListResponse> findBookChildPictureList(Integer childId) {
 
         Child child = childRepository.findById(childId)
-            .orElseThrow(() -> new IllegalArgumentException("Child Not Found"));
+                .orElseThrow(() -> new IllegalArgumentException("Child Not Found"));
 
         List<BookChildPictureListResponse> bookChildPictureListResponses = new ArrayList<>();
         List<Book> bookList = bookRepository.findByChildAndCompleteAndPictureUrlNotNullOrderByIdDesc(child, CompleteStatus.COMPLETE);
 
         for (Book book : bookList) {
             bookChildPictureListResponses.add(BookChildPictureListResponse.builder()
-                .bookId(book.getId())
-                .bookPictureUrl(book.getPictureUrl())
-                .build());
+                    .bookId(book.getId())
+                    .bookPictureUrl(book.getPictureUrl())
+                    .build());
         }
 
         return bookChildPictureListResponses;
@@ -112,28 +116,28 @@ public class BookService {
     public BookStoryDetailResponse findBookStory(Integer bookId) {
 
         Book book = bookRepository.findById(bookId)
-            .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
+                .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
 
         List<PageListResponse> pageList = book.getPageList().stream()
-            .map(page -> PageListResponse.builder()
-                .pageId(page.getId())
-                .pageStory(page.getStory())
-                .build())
-            .toList();
+                .map(page -> PageListResponse.builder()
+                        .pageId(page.getId())
+                        .pageStory(page.getStory())
+                        .build())
+                .toList();
 
         return BookStoryDetailResponse.builder()
-            .userId(book.getChild().getUser().getId())
-            .bookId(book.getId())
-            .childId(book.getChild().getId())
-            .bookName(book.getName())
-            .pageList(pageList)
-            .build();
+                .userId(book.getChild().getUser().getId())
+                .bookId(book.getId())
+                .childId(book.getChild().getId())
+                .bookName(book.getName())
+                .pageList(pageList)
+                .build();
     }
 
     public BookDetailResponse findBook(Integer bookId) {
 
         Book book = bookRepository.findById(bookId)
-            .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
+                .orElseThrow(() -> new IllegalArgumentException("Book Not Found"));
 
         List<Book> bookList = bookRepository.findByChildOrderByIdDesc(book.getChild());
 
@@ -146,23 +150,23 @@ public class BookService {
         }
 
         List<PageAllListResponse> pageList = book.getPageList().stream()
-            .map(page -> PageAllListResponse.builder()
-                .pageId(page.getId())
-                .pageStory(page.getStory())
-                .pageimageUrl(page.getImageUrl())
-                .build())
-            .toList();
+                .map(page -> PageAllListResponse.builder()
+                        .pageId(page.getId())
+                        .pageStory(page.getStory())
+                        .pageimageUrl(page.getImageUrl())
+                        .build())
+                .toList();
 
         return BookDetailResponse.builder()
-            .bookId(book.getId())
-            .childId(book.getChild().getId())
-            .bookName(book.getName())
-            .bookMaker(book.getMaker())
-            .bookPictureUrl(book.getPictureUrl())
-            .bookCoverUrl(book.getCoverUrl())
-            .bookNextId(bookNextId)
-            .pageList(pageList)
-            .build();
+                .bookId(book.getId())
+                .childId(book.getChild().getId())
+                .bookName(book.getName())
+                .bookMaker(book.getMaker())
+                .bookPictureUrl(book.getPictureUrl())
+                .bookCoverUrl(book.getCoverUrl())
+                .bookNextId(bookNextId)
+                .pageList(pageList)
+                .build();
     }
 
     public Boolean createBookImage(BookCreateRequestDto request) {
@@ -174,22 +178,29 @@ public class BookService {
                 // 기능 진행1: stories/book-creation으로 POST 요청 보내기
                 String url = EXPRESS_SERVER_URL;
                 RestTemplate restTemplate = new RestTemplate();
-                ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 
-                // 응답이 정상이면 true 반환
-                if (response.getStatusCode() == HttpStatus.OK) {
-                    // 기능 진행2: 정상 응답이 오면 true 반환
-                    return true;
-                } else {
-                    // 기능 진행3: 에러 처리
-                    System.out.println("Error: " + response.getStatusCodeValue());
-                    return false;
+                try {
+                    ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+                    // 응답이 정상이면 true 반환
+                    if (response.getStatusCode() == HttpStatus.OK) {
+                        // 기능 진행2: 정상 응답이 오면 true 반환
+                        return true;
+                    } else {
+                        // 기능 진행3: 에러 처리
+                        System.out.println("Error: " + response.getStatusCodeValue());
+                        return false;
+                    }
+                } catch (final HttpClientErrorException e) {
+                    System.out.println(e.getStatusCode());
+                    System.out.println(e.getResponseBodyAsString());
                 }
+
             } catch (Exception e) {
                 // 기능 진행3: 에러 처리
                 e.printStackTrace();
                 return false;
             }
         }
+        return false;
     }
 }
