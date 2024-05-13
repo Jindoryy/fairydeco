0499eb5eec2d1880c3bd918c0913149a05886ded
김선혁 <kkn1312@naver.com>
2024-05-13 13:00:33 +0900

    [Fix] 페이지 entity 수정

--- server_main/src/main/java/com/a402/fairydeco/domain/book/service/OpenAiService.java
+++ server_main/src/main/java/com/a402/fairydeco/domain/book/service/OpenAiService.java
@@ -93,17 +93,28 @@ public class OpenAiService {
                 "양식은 JSON 포맷이야\n" +
                 "```\n\n" +
                 "{\n" +
-                "   \"등장인물\" : [\n" +
-                "    {\n" +
-                "      \"이름1\" : \"인물 정보1\",\n" +
-                "    },\n" +
-                "    {\n" +
-                "       \"이름2\" : \"인물 정보2\",\n" +
-                "    },\n" +
-                "    ........\n" +
-                "   ],\n" +
-                "   \"제목\" : ,\n" +
-                "   \"내용 배열\" : [ \"내용 1\", \"내용 2\", ... ],\n" +
+                " \"pages\": [\n" +
+                "                  {\n" +
+                "                    \"pageNumber\": 1,\n" +
+                "                    \"sceneDescription\": \"장면묘사1\",\n" +
+                "                    \"characterDescription\": \"등장인물 들의 외관적 특징1\"\n" +
+                "                    \"backgroundDescription\": \"배경묘사1\"\n" +
+                "           \"pageStory\" : \"장면 별 스토리1\"\n" +
+                "                  },\n" +
+                "                   {\n" +
+                "                    \"pageNumber\": 2,\n" +
+                "                    \"sceneDescription\": \"장면묘사2\",\n" +
+                "                    \"characterDescription\": \"등장인물 들의 외관적 특징2\"\n" +
+                "                    \"backgroundDescription\": \"배경묘사2\"\n" +
+                "           \"pageStory\" : \"장면 별 스토리2\"\n" +
+                "                  },\n" +
+                "                  .....\n" +
+                "                \"pageNumber\": 8,\n" +
+                "                \"sceneDescription\": \"장면묘사8\",\n" +
+                "                \"characterDescription\": \"등장인물 들의 외관적 특징8\"\n" +
+                "                \"backgroundDescription\": \"배경묘사8\"\n" +
+                "                \"pageStory\" : \"장면 별 스토리8\"\n "+
+                "                ]" +
                 "}\n\n" +
                 "```\n" +
                 "키워드 중에 동물이나 사람을 등장인물로 해줘.\n" +
@@ -121,14 +132,14 @@ public class OpenAiService {
         Book savedBook = bookRepository.save(book);
         // 프롬프트 나이대별 생성
         if (age.equals("Y")) {
-            prompt += "스토리는 내용 8개 정도로 한 내용은 2~3줄 정도로 짧게 구성\n" +
+            prompt += "pages는 내용 8개 정도로 한 page의 pageStory는 2~3줄 정도로 짧게 구성\n" +
                     "3~5세 아이를 위한 동화라 내용은 흥미를 유발하지만 이해도는 크게 필요없는 내용으로 써줘\n" +
                     "문맥이 어색하지 않도록, 내용이 이어지도록 구성해줘\n" +
                     "그리고 단어는 3~5세 아동들이 이해 할 수 있는 쉬운 단어를 사용하고 어투는 다정한 ~했답니다 등의 부드러운 말투를 사용해줘 말투가 고정적일 필요는 없어\n" +
                     "\n" +
                     "시뮬레이션 하고 결과물만 출력해줘 다른 멘트는 하지마";
         } else {
-            prompt += "스토리는 내용 8개 정도로 한 내용은 5~6줄 정도로 구성\n" +
+            prompt += "pages는 내용 8개 정도로 한 page의 pageStory는 5~7줄 정도로 구성\n" +
                     "6~7세 아이를 위한 동화라 내용은 창의적이고 교육적인 내용으로 작성해줘\n" +
                     "문맥이 어색하지 않도록, 내용이 이어지도록 구성해줘\n" +
                     "그리고 단어는 6~7세 아동들이 이해 할 수 있는 쉬운 단어를 사용하고 어투는 다정한 ~했답니다 등의 부드러운 말투를 사용해줘 말투가 고정적일 필요는 없어\n" +
@@ -140,56 +151,58 @@ public class OpenAiService {
         String finalPrompt = prompt;
         return CompletableFuture.supplyAsync(() -> {
             try {
-                String[] bookStories;
                 String model = model1;
                 if (age.equals("O")) {
                     model = model2;
                 }
+                JsonNode pagesNode;
                 while (true) {
                     // 나이가 어리면 model1으로
                     // 나이 많으면 model2로 실행
                     StoryRequest request = new StoryRequest(model, finalPrompt);
                     StoryResponse storyResponse = restTemplate.postForObject(apiURL, request, StoryResponse.class);
                     String story = storyResponse.getChoices().get(0).getMessage().getContent();
-                    System.out.println(story);
                     // '''json과 ''' 사이의 부분의 인덱스 찾기
-                    if(!story.substring(0,1).equals("{")){
-                        story = story.substring(8,story.length()-4);
-                        System.out.println(story);
+                    if (!story.substring(0, 1).equals("{")) {
+                        story = story.substring(8, story.length() - 4);
                     }
+                    System.out.println(story);
                     ObjectMapper objectMapper = new ObjectMapper();
                     // JSON 문자열을 JsonNode로 읽어오기
                     JsonNode jsonNode = objectMapper.readTree(story);
-                    // "내용 배열"에 해당하는 JsonNode 가져오기
-                    JsonNode contentArrayNode = jsonNode.get("내용 배열");
+                    // "pages"에 해당하는 JsonNode 가져오기
+                    pagesNode = jsonNode.get("pages");
                     // JsonNode를 배열로 변환
-                    bookStories = objectMapper.treeToValue(contentArrayNode, String[].class);
-
-                    // JSON 문자열을 JsonElement로 파싱
-//                    JsonElement jsonElement = JsonParser.parseString(story);
-//                    // JsonElement에서 JsonObject 추출
-//                    JsonObject jsonObject = jsonElement.getAsJsonObject();
-//                    // "내용 배열"에 해당하는 JsonArray 추출
-//                    JsonArray storyList = jsonObject.getAsJsonArray("내용 배열");
-                    // JsonArray를 배열로 변환
-//                    bookStories = new Gson().fromJson(storyList, String[].class);
-                    System.out.println(bookStories.length);
-                    if (bookStories.length < 8) {
+                    System.out.println(pagesNode.size());
+                    if (pagesNode.size() < 8) {
                         continue;
-                    }
-
-                    for (String tmp : bookStories) {
-                        System.out.println(tmp);
                     }
                     break;
                 }
+                String[] sceneDescriptions = new String[pagesNode.size()];
+                String[] characterDescriptions = new String[pagesNode.size()];
+                String[] backgroundDescriptions = new String[pagesNode.size()];
+                String[] pageStories = new String[pagesNode.size()];
+                // pages의 배열에서 각각 인자 배열에 저장
+                for (int i = 0; i < pagesNode.size(); i++) {
+                    JsonNode pageNode = pagesNode.get(i);
+                    // 각각 인자에 넣음
+                    sceneDescriptions[i] = pageNode.get("sceneDescription").asText();
+                    characterDescriptions[i] = pageNode.get("characterDescription").asText();
+                    backgroundDescriptions[i] = pageNode.get("backgroundDescription").asText();
+                    pageStories[i] = pageNode.get("pageStory").asText();
+                    System.out.println(pageStories[i]);
+                }
                 // 각 page 8개 db에 저장하는 작업
-                for (int i = 0; i < bookStories.length; i++) {
+                for (int i = 0; i < pageStories.length; i++) {
                     // 목소리 파일 생성 후 s3 저장
-                    File voice = voiceUtil.createVoice(bookStories[i]);
+                    File voice = voiceUtil.createVoice(pageStories[i]);
                     Page page = Page.builder()
                             .book(savedBook)
-                            .story(bookStories[i])
+                            .story(pageStories[i])
+                            .sceneDescription(sceneDescriptions[i])
+                            .characterDescription(characterDescriptions[i])
+                            .backgroundDescription(backgroundDescriptions[i])
                             .voiceUrl(fileUtil.uploadMP3(voice))
                             .voiceDuration(voiceUtil.getVoiceDuration(voice))
                             .build();
