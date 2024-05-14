7b8286f399b4692d79085883d0920ee8814b4779
youngjin <poweryj1@naver.com>
2024-05-14 17:16:47 +0900

    [Feat] 퍼즐 디자인 중

--- client/src/app/puzzle/page.jsx
+++ client/src/app/puzzle/page.jsx
@@ -2,12 +2,26 @@
 
 import React, { useState, useEffect, useRef } from 'react'
 import * as headbreaker from 'headbreaker'
+import { useRouter } from 'next/navigation'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import NewImage from 'next/image'
+import WallImage from '../../../public/image/wall.jpg'
+import NextImage from '../../../public/image/puzzle-next.png'
+import AnswerImage from '../../../public/image/puzzle-answer.png'
+import SketchImage from '../../../public/image/puzzle-sketch.png'
+import { ArrowCircleLeft } from '@phosphor-icons/react/dist/ssr'
 
 function DemoJigsaw(props) {
+    const Swal = require('sweetalert2')
     const puzzleRef = useRef(null)
     const [canvas, setCanvas] = useState(null)
     const [resetTrigger, setResetTrigger] = useState(0)
     const [shuffleTrigger, setShuffleTrigger] = useState(0)
+    const [sketchOpacity, setSketchOpacity] = useState(0.55)
+    const router = useRouter()
+
+    //api
 
     useEffect(() => {
         const img = new Image()
@@ -46,11 +60,19 @@ function DemoJigsaw(props) {
             newCanvas.attachSolvedValidator()
             newCanvas.onValid(() => {
                 setTimeout(() => {
-                    if (confirm('정답입니다! 다음 퍼즐도 풀어볼까요?')) {
-                        alert('다음으로')
-                    } else {
-                        alert('안할래')
-                    }
+                    Swal.fire({
+                        title: '정답입니다! 다음 퍼즐도 풀어볼까요?',
+                        icon: 'success',
+                        confirmButtonText: '네!',
+                        showDenyButton: true,
+                        denyButtonText: `아니오`
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                          Swal.fire("Saved!");
+                        } else if (result.isDenied) {
+                          Swal.fire("Changes are not saved");
+                        }
+                    });
                 }, 500)
             })
 
@@ -70,6 +92,7 @@ function DemoJigsaw(props) {
         resetTrigger,
     ])
 
+    //다시 섞고 싶을 때
     useEffect(() => {
         if (shuffleTrigger > 0 && canvas) {
             canvas.shuffle(0.5)
@@ -77,39 +100,88 @@ function DemoJigsaw(props) {
         }
     }, [shuffleTrigger, canvas])
 
-    const resetPuzzle = () => {
-        setShuffleTrigger(0)
-        setResetTrigger((prev) => prev + 1)
-    }
-
     const shufflePuzzle = () => {
         setShuffleTrigger((prev) => prev + 1)
     }
 
+    const resetPuzzle = () => {
+        setShuffleTrigger(0) 
+        setResetTrigger((prev) => prev + 1)
+    }
+    
+    const sketchToggle = () => {
+        setSketchOpacity(prevOpacity => prevOpacity === 0.55 ? 0.01 : 0.55)
+    }
+
+    const goBack = () => {
+        router.push('/')
+    }
+
     return (
-        <>
-            <button
-                onClick={resetPuzzle}
-                style={{ fontSize: '16px', padding: '10px 20px' }}
-            >
-                정답
-            </button>
-            <button
-                onClick={shufflePuzzle}
-                style={{ fontSize: '16px', padding: '10px 20px' }}
-            >
-                섞기
-            </button>
-            <div style={{ textAlign: 'center', margin: '20px 60px 0px' }}>
+        <>  
+            <div style={{ width: '80%', display:'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <button
+                    className="btn btn-ghost relative ml-2 h-auto pt-2 align-middle text-lg font-thin text-white hover:bg-transparent focus:bg-transparent"
+                    onClick={goBack}
+                >
+                    <ArrowCircleLeft
+                        size={80}
+                        weight="fill"
+                        className="text-black"
+                    />
+                </button>
+                <div style={{ fontSize: '2.6rem', marginLeft: '100px' }}>
+                    퍼즐놀이
+                </div>
+                <div>
+                    <button
+                        onClick={resetPuzzle}
+                        style={{ width: '100px' }}
+                    >
+                        <NewImage
+                            src={AnswerImage}
+                            alt={'go to next puzzle'}
+                        />
+                        <span >
+                            정답 보기
+                        </span>
+                    </button>
+                    <button
+                        onClick={sketchToggle}
+                        style={{ width: '100px' }}
+                    >
+                        <NewImage
+                            src={SketchImage}
+                            alt={'go to next puzzle'}
+                        />
+                        <span>
+                            힌트 보기
+                        </span>
+                    </button>
+                    <button
+                        onClick={shufflePuzzle}
+                        style={{ width: '100px' }}
+                    >
+                        <NewImage
+                            src={NextImage}
+                            alt={'go to next puzzle'}
+                        />
+                        <span >
+                            다음 퍼즐
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                 <div
                     ref={puzzleRef}
                     id={props.id}
-                    style={{ marginBottom: '20px', position: 'absolute', border: '2px solid black' }}
+                    style={{ position: 'relative', border: '2px solid black' }}
                 >
                 </div>
                 <img 
-                    src="https://fairydecos3.s3.ap-northeast-2.amazonaws.com/storybook-images/198-title.png"
-                    style={{ float: 'right', opacity: '0.5', position: 'relative', zIndex: '-1', margin: '45px' }}
+                    src={props.imageSrc}
+                    style={{ position: 'absolute',opacity: sketchOpacity, zIndex: '-1', left: '55%', border: '15px solid #790000' }}
                 /> 
             </div>
         </>
@@ -122,7 +194,10 @@ export default function Puzzle() {
     const [height, setHeight] = useState(600)
 
     return (
-        <main>
+        <div 
+            className="h-dvh w-dvw bg-customYellow"
+            style={{ zIndex: '-2', position: 'absolute', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+        >
             <DemoJigsaw
                 id="puzzle"
                 pieceSize={pieceSize}
@@ -130,6 +205,6 @@ export default function Puzzle() {
                 height={height}
                 imageSrc="https://fairydecos3.s3.ap-northeast-2.amazonaws.com/storybook-images/198-title.png"
             />
-        </main>
+        </div>
     )
 }
